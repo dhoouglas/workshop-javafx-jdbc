3e80e75adcb2ff019522c346abd56a0a438a8fc2
unknown <dhoouglas@hotmail.com>
2024-01-31 12:28:27 -0300

    Adding database access

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,16 @@
+package model.dao;
+
+import db.DB;
+import model.dao.impl.DepartmentJDBC;
+import model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
+	public static DepartmentDao createDepartementDao() {
+		return new DepartmentJDBC(DB.getConnection());
+	}
+}
